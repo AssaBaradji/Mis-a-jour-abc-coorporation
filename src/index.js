3e51f72896c6698cbc1c58e7e439d1232e63f447
AssaBaradji <baradjiassa695@gmail.com>
2024-09-11 19:45:26 +0200

    ajout de la gestion des commandes et datails

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const readlineSync = require("readline-sync");
 const customerManager = require("./customerManager");
 const productManager = require("./productManager");
+const orderManager = require("./orderManager");
 const paymentManager = require("./paymentManager");
+
+function isValidDate(dateString) {
+  const date = new Date(dateString);
+  return date.toISOString().startsWith(dateString);
+}
 
 async function customerMenu() {
   let choice = "";
@@ -137,6 +143,276 @@ async function productMenu() {
       case "0":
         console.log("Retour au menu principal.");
         break;
+
+      default:
+        console.log("Choix invalide.");
+    }
+  }
+}
+
+async function orderDetailMenu() {
+  let choice = "";
+  while (choice !== "0") {
+    console.log("\n--- Gestion des détails de commande ---");
+    console.log("1. Ajouter des produits à la commande");
+    console.log("2. Sauvegarder et quitter");
+    console.log("0. Retour au menu principal");
+
+    choice = readlineSync.question("Entrez votre choix : ");
+
+    switch (choice) {
+      case "1":
+        const productId = readlineSync.question("ID du produit : ");
+        let quantity = readlineSync.question("Quantité : ");
+        const price = readlineSync.question("Prix : ");
+
+        if (!productId || isNaN(productId)) {
+          console.log("Erreur : L'ID du produit est invalide.");
+          break;
+        }
+
+        if (!quantity || isNaN(quantity) || quantity <= 0) {
+          console.log(
+            "Erreur : La quantité doit être un nombre valide supérieur à 0."
+          );
+          break;
+        }
+
+        if (!price || isNaN(price) || price <= 0) {
+          console.log(
+            "Erreur : Le prix doit être un nombre valide supérieur à 0."
+          );
+          break;
+        }
+        try {
+          await orderManager.addProductToOrder(productId, quantity, price);
+          console.log("Produit ajouté à la commande avec succès.");
+        } catch (error) {
+          console.error(
+            "Erreur lors de l'ajout du détail de commande : " + error.message
+          );
+        }
+        break;
+
+      case "2":
+        console.log("Commande ajoutée avec succès avec l'ID :", newOrderId);
+        return;
+
+      case "0":
+        console.log("Retour au menu principal.");
+        break;
+
+      default:
+        console.log("Choix invalide.");
+    }
+  }
+}
+
+async function orderMenu() {
+  let choice = "";
+  while (choice !== "0") {
+    console.log("\n--- Gestion des commandes ---");
+    console.log("1. Ajouter une commande avec ses détails");
+    console.log("2. Lister les commandes avec les détails");
+    console.log("3. Mettre à jour une commande avec ses détails");
+    console.log("4. Supprimer une commande avec ses détails");
+    console.log("0. Retour au menu principal");
+
+    choice = readlineSync.question("Entrez votre choix : ");
+
+    switch (choice) {
+      case "1":
+        const date = readlineSync.question("Date (YYYY-MM-DD) : ");
+        const deliveryAddress = readlineSync.question(
+          "Adresse de livraison : "
+        );
+        const trackNumber = readlineSync.question("Numéro de suivi : ");
+        const status = readlineSync.question("Statut (en cours/complété) : ");
+        const customerId = readlineSync.question("ID du client : ");
+
+        if (!isValidDate(date)) {
+          console.log(
+            "Date invalide. Veuillez entrer une date au format YYYY-MM-DD."
+          );
+          break;
+        }
+
+        try {
+          const customerExists = await customerManager.getById(customerId);
+          if (!customerExists) {
+            console.log(
+              `Erreur : Le client avec l'ID ${customerId} n'existe pas.`
+            );
+            break;
+          }
+
+          const newOrderId = await orderManager.addOrder(
+            date,
+            deliveryAddress,
+            trackNumber,
+            status,
+            customerId
+          );
+
+          await orderDetailMenu(newOrderId);
+        } catch (error) {
+          console.error(
+            "Erreur lors de l'ajout de la commande :",
+            error.message
+          );
+        }
+        break;
+
+      case "2":
+        try {
+          const orders = await orderManager.getOrders();
+          for (const order of orders) {
+            const { order: orderData, details } =
+              await orderManager.getOrderDetails(order.id);
+
+            const orderObject = {
+              id: orderData.id,
+              date: orderData.date,
+              delivery_address: orderData.delivery_address,
+              track_number: orderData.track_number,
+              status: orderData.status,
+              customer_id: orderData.customer_id,
+              details: details.map((detail) => ({
+                product_id: detail.product_id,
+                quantity: detail.quantity,
+                price: detail.price,
+              })),
+            };
+
+            console.log("Commande:", JSON.stringify(orderObject, null, 2));
+          }
+        } catch (error) {
+          console.error(
+            "Erreur lors de la récupération des commandes :",
+            error.message
+          );
+        }
+        break;
+
+      case "3":
+        const updateId = readlineSync.question(
+          "ID de la commande à mettre à jour : "
+        );
+
+        const newDate = readlineSync.question("Nouvelle date (YYYY-MM-DD) : ");
+        const newDeliveryAddress = readlineSync.question(
+          "Nouvelle adresse de livraison : "
+        );
+        const newTrackNumber = readlineSync.question(
+          "Nouveau numéro de suivi : "
+        );
+        const newStatus = readlineSync.question("Nouveau statut : ");
+        const newCustomerId = readlineSync.question("Nouvel ID du client : ");
+
+        try {
+          if (!isValidDate(newDate)) {
+            console.log(
+              "Date invalide. Veuillez entrer une date au format YYYY-MM-DD."
+            );
+            break;
+          }
+
+          const affectedRows = await orderManager.updateOrder(
+            updateId,
+            newDate,
+            newDeliveryAddress,
+            newTrackNumber,
+            newStatus,
+            newCustomerId
+          );
+
+          if (affectedRows > 0) {
+            console.log("Commande mise à jour avec succès.");
+          } else {
+            console.log("Aucune commande trouvée avec cet ID.");
+          }
+
+          const currentDetails = await orderManager.getOrderDetails(updateId);
+
+          for (const detail of currentDetails.details) {
+            console.log(
+              `Produit ID: ${detail.product_id}, Quantité actuelle: ${detail.quantity}, Prix actuel: ${detail.price}`
+            );
+
+            const updateDetail = readlineSync.question(
+              "Voulez-vous mettre à jour ce détail (o/n) ? "
+            );
+
+            if (updateDetail.toLowerCase() === "o") {
+              const newQuantity = readlineSync.question(
+                `Nouvelle quantité pour le produit ${detail.product_id} : `
+              );
+              const newPrice = readlineSync.question(
+                `Nouveau prix pour le produit ${detail.product_id} : `
+              );
+
+              await orderManager.updateOrderDetail(
+                detail.id,
+                newQuantity,
+                newPrice
+              );
+              console.log(`Détail du produit ${detail.product_id} mis à jour.`);
+            }
+          }
+
+          const addMoreDetails = readlineSync.question(
+            "Voulez-vous ajouter un nouveau produit à cette commande (o/n) ? "
+          );
+          if (addMoreDetails.toLowerCase() === "o") {
+            const newProductId = readlineSync.question(
+              "ID du nouveau produit : "
+            );
+            const newProductQuantity = readlineSync.question(
+              "Quantité du nouveau produit : "
+            );
+            const newProductPrice = readlineSync.question(
+              "Prix du nouveau produit : "
+            );
+
+            await orderManager.addOrderDetail(
+              updateId,
+              newProductId,
+              newProductQuantity,
+              newProductPrice
+            );
+            console.log("Nouveau produit ajouté à la commande.");
+          }
+        } catch (error) {
+          console.error(
+            "Erreur lors de la mise à jour de la commande :",
+            error.message
+          );
+        }
+        break;
+
+      case "4":
+        const deleteId = readlineSync.question(
+          "ID de la commande à supprimer : "
+        );
+
+        try {
+          const affectedRows = await orderManager.destroyOrder(deleteId);
+          if (affectedRows > 0) {
+            console.log("Commande supprimée avec succès.");
+          } else {
+            console.log("Aucune commande trouvée avec cet ID.");
+          }
+        } catch (error) {
+          console.error(
+            "Erreur lors de la suppression de la commande :",
+            error.message
+          );
+        }
+        break;
+
+      case "0":
+        console.log("Retour au menu principal.");
+        return;
 
       default:
         console.log("Choix invalide.");
